9a924d446729d3646b265467c86432336a2a8afe
BreezeLA <breezelan_2002@163.com>
2024-07-29 17:18:03 +0800

    用户注册功能

--- js/common.js
+++ js/common.js
@@ -1,1 +1,13 @@
 axios.defaults.baseURL = 'https://hmajax.itheima.net/api/';
+
+// message提示
+function showToast(message) {
+  // 获取toast元素
+  const toastDom = document.querySelector('.toast');
+  // 设置提示内容
+  document.querySelector('.toast-body').innerText = message;
+  // 创建toast
+  const toast = new bootstrap.Toast(toastDom);
+  // 显示toast
+  toast.show();
+};
